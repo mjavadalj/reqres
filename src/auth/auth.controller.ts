da2780b5367d69cda40b7b2fce2d1751dc31d347
mjavad <mjavad.alj@yahoo.com>
2023-08-17 09:38:07 +0430

    redis reset when signing up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,16 +7,27 @@ import {
   Post,
   Put,
   Logger,
+  Inject,
 } from '@nestjs/common';
 import { AuthDto } from './auth.dto';
 import { AuthService } from './auth.service';
 import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { User, UserDocument } from '../user/user.schema';
+import { Cache } from 'cache-manager';
+import {
+  CacheInterceptor,
+  CacheTTL,
+  CacheKey,
+  CACHE_MANAGER,
+} from '@nestjs/cache-manager';
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   private logger = new Logger('AuthController');
-  constructor(private readonly service: AuthService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+    private readonly service: AuthService,
+  ) {}
   @Post('signup')
   @ApiResponse({ status: 200, description: 'sign up successful.' })
   @ApiResponse({ status: 400, description: 'data not valid.' })
@@ -26,6 +37,7 @@ export class AuthController {
   })
   async singup(@Body() signupDto: AuthDto) {
     this.logger.verbose(`UserDto: ${JSON.stringify(signupDto)} signing up`);
+    await this.cacheService.reset();
     return await this.service.signup(signupDto);
   }
   @Post('login')
@@ -37,6 +49,7 @@ export class AuthController {
   })
   async login(@Body() loginDto: AuthDto): Promise<{ token: string }> {
     this.logger.verbose(`UserDto: ${JSON.stringify(loginDto)} logging in`);
+    await this.cacheService.reset();
     return await this.service.login(loginDto);
   }
 }
